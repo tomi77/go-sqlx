4d89fbac445290181764b21a207e740468e86b1c
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2018-11-16 15:01:47 +0100

    rename NullTime to NullDateTime

--- date-time.go
+++ date-time.go
@@ -5,20 +5,20 @@ import (
 	"time"
 )
 
-// NullTime represents an time.Time that may be null. NullTime implements the Scanner interface so it can be used as a scan destination, similar to NullString.
-type NullTime struct {
+// NullDateTime represents an time.Time that may be null. NullDateTime implements the Scanner interface so it can be used as a scan destination, similar to NullString.
+type NullDateTime struct {
 	Time  time.Time
 	Valid bool // Valid is true if Time is not NULL
 }
 
 // Scan implements the Scanner interface.
-func (n *NullTime) Scan(value interface{}) error {
+func (n *NullDateTime) Scan(value interface{}) error {
 	n.Time, n.Valid = value.(time.Time)
 	return nil
 }
 
 // Value implements the driver Valuer interface.
-func (n NullTime) Value() (driver.Value, error) {
+func (n NullDateTime) Value() (driver.Value, error) {
 	if !n.Valid {
 		return nil, nil
 	}
